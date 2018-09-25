1aedcc7f11b51792eb18c38f5ba32e4c4bdcb622
Jesus <jesusadadfilho@gmail.com>
2018-09-24 22:23:54 -0300

    ok

--- Questao1.py
+++
@@ -1,5 +0,0 @@
-import requests
-response = requests.get('http://www.google.com')
-print(response.status_code)
-print(response.headers['content-type'])
-print(response.text)
--- Questao2.py
+++
@@ -1,3 +0,0 @@
-import urllib.request
-url = input('digite Url: ')
-urllib.request.urlretrieve(url,"miranha.jpg")
